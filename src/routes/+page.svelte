3dd9dd01b59c01ac48fb26ba0b6b7e8ee5ed3ab1
Tomasz Dobrowolski <chrumck@gmail.com>
2025-05-29 11:38:37 +0200

    Set up for static inline html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+  const value = "/tutorial/image.gif";
+</script>
+
+<svelte:head>
+  <style>
+    p {
+      color: goldenrod;
+      font-family: "Comic Sans MS", cursive;
+      font-size: 2em;
+    }
+  </style>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">{value}</a> to read the documentation
+</p>
