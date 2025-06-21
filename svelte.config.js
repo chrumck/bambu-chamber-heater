fd782ab4300c5d9a8456de14b8e4d0fd2722c08f
Tomasz Dobrowolski <chrumck@gmail.com>
2025-06-21 13:15:31 +0200

    move fronted source files to src_front

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,18 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({ fallback: "index.html" }),
     output: {
       bundleStrategy: "inline",
+    },
+    files: {
+      assets: "static",
+      lib: "src_front/lib",
+      routes: "src_front/routes",
+      appTemplate: "src_front/app.html",
     },
   },
 };
